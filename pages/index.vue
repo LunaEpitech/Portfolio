73b30107299dd3636023dd020f675d49b8753ed9
LunaEpitech <lilian.carausse@epitech.eu>
2022-12-01 15:13:23 +0100

    Missing an argument

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="mainBox">
+        <p>Le <a href="https://thomy-lorenzatti.notion.site/thomy-lorenzatti/Pr-sentation-JAM-Portfolio-1e8c57ecb4044871bb57198238500928" target="_blank">Notion</a></p>
+        <p>Documentation <a href="https://v3.nuxtjs.org/" target="_blank">Nuxt 3</a></p>
+        <p>Documentation <a href="https://vuejs.org/" target="_blank">Vuejs</a></p>
+    </div>
+</template>
+
+<style scoped>
+
+.mainBox {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100vw;
+    height: 100vh;
+}
+a {
+    text-decoration: underline;
+}
+</style>
