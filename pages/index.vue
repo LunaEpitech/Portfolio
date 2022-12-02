040b75506427e3929ca0ee7119a0a9e3528ab217
LunaEpitech <lilian.carausse@epitech.eu>
2022-12-02 15:40:33 +0100

    Missing an argument

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,17 @@
 <template>
-    <header>
-        <p>Working towards a better future</p>
-    </header>
-    <li v-for="data in data" :key="data.name">
-        <div class="Repos">
-            <p>{{data.name}}</p>
-            <a :href="data.html_url"><br></a>
-        </div>
-    </li>
+    <div class="Repos">
+        <li v-for="data in data" :key="data.name">
+            <a :href="data.html_url">{{data.name}}<br></a>
+        </li>
+    </div>
     <div class="mainBox">
         <p>Le <a href="https://thomy-lorenzatti.notion.site/thomy-lorenzatti/Pr-sentation-JAM-Portfolio-1e8c57ecb4044871bb57198238500928" target="_blank">Notion</a></p>
         <p>Documentation <a href="https://v3.nuxtjs.org/" target="_blank">Nuxt 3</a></p>
         <p>Documentation <a href="https://vuejs.org/" target="_blank">Vuejs</a></p>
     </div>
+    <header>
+        <p>Working towards a better future</p>
+    </header>
     <footer>
         <p>  ©2022</p>
     </footer>
@@ -23,6 +22,34 @@
 </script>
 
 <style scoped>
+
+.mainBox {
+    right: 0;
+    display: flex;
+    float:right;
+    color:rgb(229, 184, 244);
+    text-shadow: 1px 1px 1px rgb(193, 71, 233);
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
+    position:fixed;
+    font-size: 30px;
+    width: 30vw;
+    height: 100vh;
+    background-color: rgb(45, 3, 59);
+}
+
+.Repos {
+    display:flex;
+    float:left;
+    left: 5%;
+    justify-content: center;
+    color:rgb(229, 184, 244);
+    text-shadow: 1px 1px 1px rgb(193, 71, 233);
+    background-color: rgb(45, 3, 59);
+    width: 100vw;
+    height: 100vh;
+}
 
 header {
     position: fixed;
@@ -50,28 +77,6 @@ footer {
     background: linear-gradient(0deg, rgb(129, 12, 168) 70%, rgba(0, 0, 0, 0) 100%);
 }
 
-.Repos {
-    display:flex;
-    float:left;
-    justify-content: center;
-    color:rgb(229, 184, 244);
-    text-shadow: 1px 1px 1px rgb(193, 71, 233);
-    background-color: rgb(45, 3, 59);
-    width: 25%;
-    height: 100vh;
-}
-.mainBox {
-    display: flex;
-    float:right;
-    color:rgb(229, 184, 244);
-    text-shadow: 1px 1px 1px rgb(193, 71, 233);
-    flex-direction: column;
-    justify-content: center;
-    font-size: 30px;
-    width: 40%;
-    height: 100vh;
-    background-color: rgb(45, 3, 59);
-}
 a {
     text-decoration: underline;
 }
